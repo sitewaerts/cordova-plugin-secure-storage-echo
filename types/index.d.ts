1c763e4186cff968536fbd0fde5752794ef2d30c
schauerte <schauerte@sitewaerts.de>
2023-11-06 11:29:46 +0100

    types

---
+++ types/index.d.ts
@@ -0,0 +1,55 @@
+// Type definitions for Apache Cordova Secure Storage Echo plugin
+// Project: https://github.com/sitewaerts/cordova-plugin-secure-storage-echo
+// Definitions by: Microsoft Open Technologies Inc. <http://msopentech.com>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+//
+// Copyright (c) Microsoft Open Technologies Inc
+// Licensed under the MIT license
+
+interface SecureStorageOptionsOSSpecific {
+
+}
+
+interface SecureStorageOptionsAndroid extends SecureStorageOptionsOSSpecific{
+
+}
+
+interface SecureStorageOptionsElectron extends SecureStorageOptionsOSSpecific{
+
+}
+
+interface SecureStorageOptionsIOS extends SecureStorageOptionsOSSpecific{
+
+}
+
+interface SecureStorageOptionsWindows extends SecureStorageOptionsOSSpecific{
+
+}
+
+interface SecureStorageOptions {
+    android?: SecureStorageOptionsAndroid
+    electron?: SecureStorageOptionsElectron
+    ios?: SecureStorageOptionsIOS
+    windows?: SecureStorageOptionsWindows
+}
+
+interface SecureStorage {
+
+    get: (success:(value:string)=>void, error:(error:any)=>void, key:string)=>void
+
+    set: (success:(key:string)=>void, error:(error:any)=>void, key:string, value:string|null)=>void
+
+    remove: (success:(key:string)=>void, error:(error:any)=>void, key:string)=>void
+
+    keys: (success:(keys:Array<string>)=>void, error:(error:any)=>void)=>void
+
+    clear: (success:()=>void, error:(error:any)=>void)=>void
+}
+
+declare var SecureStorage: {
+    new (success:()=>void, error:(error:any)=>void, service:string, options:SecureStorageOptions): SecureStorage;
+};
+
+interface CordovaPlugins{
+    SecureStorage: SecureStorage
+}
